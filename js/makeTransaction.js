bb7c408ed054add93ad126ed42da663827bf5285
AlesiaZuk <78986532+AlesiaZuk@users.noreply.github.com>
2021-09-12 14:03:13 +0300

    Update makeTransaction.js

--- js/makeTransaction.js
+++ js/makeTransaction.js
@@ -4,15 +4,11 @@ const randomIntegerFromInterval = (min, max) => {
 
 const makeTransaction = ({ id }) => {
   const delay = randomIntegerFromInterval(200, 500);
-  console.log(delay);
-
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const canProcess = Math.random() > 0.3;
-      console.log(canProcess);
-
       if (canProcess) {
-        resolve(id, delay);
+        resolve({ id, delay });
       } else {
         reject(id);
       }
@@ -20,18 +16,14 @@ const makeTransaction = ({ id }) => {
   });
 };
 
-const logSuccess = (id, time) => {
-  console.log(`Transaction ${id} processed in ${time}ms`);
+const logSuccess = ({ id, delay }) => {
+  console.log(`Transaction ${id} processed in ${delay}ms`);
 };
-
 const logError = (id) => {
   console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
 
 makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
-
 makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
-
 makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
-
 makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
